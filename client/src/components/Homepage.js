4937e99c8f2042b6d3bdb5a8abd64bc9391f375a
Swetha Reddy <shwetha.reddy0@gmail.com>
2022-12-12 10:16:19 -0800

    add cards to the homepage

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,12 +1,40 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Card, ListGroup } from "react-bootstrap";
 
 // Home page of the app
 function Homepage() {
   return (
-    <div>
-      <h1>Cards</h1>
-    </div>
+    <>
+      <Card border="dark" style={{ width: "18rem" }}>
+        <Card.Header>Top 5 Cities in the USA</Card.Header>
+        <Card.Body>
+          <Card.Text>
+            <ListGroup variant="flush">
+              <ListGroup.Item>Atlanta</ListGroup.Item>
+              <ListGroup.Item>California</ListGroup.Item>
+              <ListGroup.Item>Las Vegas</ListGroup.Item>
+              <ListGroup.Item>Portland</ListGroup.Item>
+            </ListGroup>
+          </Card.Text>
+          <Card.Link href="#">Search City</Card.Link>
+        </Card.Body>
+      </Card>
+
+      <Card border="dark" style={{ width: "18rem" }}>
+        <Card.Header>Top 5 States in the USA</Card.Header>
+        <Card.Body>
+          <Card.Text>
+            <ListGroup variant="flush">
+              <ListGroup.Item>Atlanta</ListGroup.Item>
+              <ListGroup.Item>California</ListGroup.Item>
+              <ListGroup.Item>Florida</ListGroup.Item>
+              <ListGroup.Item>Idaho</ListGroup.Item>
+            </ListGroup>
+          </Card.Text>
+          <Card.Link href="#">Search City</Card.Link>
+        </Card.Body>
+      </Card>
+    </>
   );
 }
 
