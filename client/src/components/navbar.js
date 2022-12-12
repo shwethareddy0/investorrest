5003cf1e01369ad1a04096a78b6dee5791a07ce0
Mrwaynejames <mcwilliamsjack96@gmail.com>
2022-12-12 13:58:06 -0800

    gave avtive class

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import Auth from "../utils/auth";
+import "../App.scss"
 
 function AppNavbar() {
   return (
@@ -14,29 +15,29 @@ function AppNavbar() {
           <Navbar.Toggle aria-controls="navbar" />
           <Navbar.Collapse id="navbar">
             <Nav className="ml-auto">
-              <Nav.Link as={Link} to="/compare">
+              <Nav.Link activeclassname="active" as={Link} to="/compare">
                 <h4>Compare Property</h4>
               </Nav.Link>
-              <Nav.Link as={Link} to="/search">
+              <Nav.Link activeclassname="active" as={Link} to="/search">
                 <h4>Search City</h4>
               </Nav.Link>
-              <Nav.Link as={Link} to="/myhomes">
+              <Nav.Link activeclassname="active" as={Link} to="/myhomes">
                 <h4>My Homes</h4>
               </Nav.Link>
               {/* if user is logged in show saved homes and logout */}
               {Auth.loggedIn() ? (
                 <>
-                  <Nav.Link as={Link} to="/saved">
+                  <Nav.Link activeclassname="active" as={Link} to="/saved">
                     <h4>My Homes</h4>
                   </Nav.Link>
                   <Nav.Link onClick={Auth.logout}>Logout</Nav.Link>
                 </>
               ) : (
                 <>
-                  <Nav.Link as={Link} to="/login">
+                  <Nav.Link activeclassname="active" as={Link} to="/login">
                     <h4>Login</h4>
                   </Nav.Link>
-                  <Nav.Link as={Link} to="/signup">
+                  <Nav.Link activeclassname="active" as={Link} to="/signup">
                     <h4>Signup</h4>
                   </Nav.Link>
                 </>
