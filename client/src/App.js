7305d5c506e8a2a912307c44fa280fce78a6962e
Swetha Reddy <shwetha.reddy0@gmail.com>
2022-12-11 23:56:52 -0800

    add traditionalcomparablelisting component and update comparaeproperty page

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
--- client/src/pages/CompareProperty.js
+++ client/src/pages/CompareProperty.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import TraditionalComparableListing from "../components/TranditionalComparableListing";
 
+// 397651 - property id as an example
 function CompareProperty() {
+  const [ready, setReady] = useState(false);
   return (
     <div>
-      <h1>CompareProperty</h1>
+      <button onClick={() => setReady(true)}>Ready</button>
+      {ready && <TraditionalComparableListing propertyId={397651} />}
     </div>
   );
 }
