2cd054556eb1f2748af99e2c36783e59cc4d3f2a
Mrwaynejames <mcwilliamsjack96@gmail.com>
2022-12-11 17:38:44 -0800

    city stats

--- client/src/components/city paage/index.js
+++ client/src/components/city paage/index.js
@@ -1,24 +1,47 @@
-return (
-    <section className="container-fluid">
-        <div className="card col-4">
-            <form>
-            <div class="form-group">
-                <label for="inputAddress">Address</label>
-                <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-            </div>
-            <div class="form-row">
-                <div class="form-group col-md-5">
-                <label for="inputCity">City</label>
-                <input type="text" class="form-control" id="inputCity">
+const City = () => {
+    return (
+        <section className="container-fluid">
+        <div className = "row">
+            <div className="card col-4">
+                <form>
+                <div className="form-group">
+                    <div className="form-group col-md-5">
+                    <label for="inputCity">City</label>
+                    <input type="text" className="form-control" id="inputCity"/>
+                    </div>
+                    <div className="form-group col-md-5">
+                    <label for="inputState">State</label>
+                    <input type="text" className="form-control" id="inputState" required />
+                    </div>
                 </div>
-                <div class="form-group col-md-5">
-                <label for="inputState">State</label>
-                <input type="text" class="form-control" id="inputState">
+                <div className="form-group">
+                <button type="submit" className="btn btn-primary">Sign in</button>
+                </div>
+                </form> 
+            </div>
+
+            <div className="col-lg-6 mb-4">
+                <div className="card">
+                    <div className="card-body">
+                        <h3 className="card-title">Area Stats</h3>
+                        <h4 className="areaStat"> Average Occupency Rate:</h4>
+                            <h5 className="abbStat">"${airbnb.occupencyRate}"</h5> 
+                        <h4 className="areaStat"> Number of Airbnbs:</h4>
+                            <h5 className="abbStat">"${airbnb.occupencyRate}"</h5> 
+                        <h4 className="areaStat"> Average Nighlty Rate:</h4>
+                            <h5 className="abbStat">"${airbnb.occupencyRate}"</h5>
+                        <h4 className="areaStat"> Average Monthly Earning:</h4>
+                            <h5 className="abbStat">"${airbnb.occupencyRate}"</h5>
+                        <h4 className="areaStat"> Average Property Price:</h4>
+                            <h5 className="abbStat">"${airbnb.occupencyRate}"</h5>   
+                        <a href="#" className="btn btn-outline-secondary btn-sm">
+                            <i className="far fa-heart"></i></a>
+                    </div>
                 </div>
             </div>
-            <div class="form-group">
-            <button type="submit" class="btn btn-primary">Sign in</button>
-            </form> 
         </div>
-    </section>
-);
+        </section>
+    );
+};
+
+export default City;
