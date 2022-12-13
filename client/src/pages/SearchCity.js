e278e0693b82c19f480924c70eff5dfb6e240265
Swetha Reddy <shwetha.reddy0@gmail.com>
2022-12-13 12:10:28 -0800

    update the Search city page with api

--- client/src/pages/SearchCity.js
+++ client/src/pages/SearchCity.js
@@ -1,63 +1,111 @@
 // import { faUnderline } from "@fortawesome/free-solid-svg-icons";
-import React from "react";
+import React, { useState } from "react";
 //  import Button from 'react-bootstrap/Button';
 //  import Form from 'react-bootstrap/Form';
-import { Card, Form, Button, Container, Row, Col }from 'react-bootstrap';
+import { Card, Form, Button, Container, Row, Col } from "react-bootstrap";
+//import TraditionalComparableListing from "../components/TranditionalComparableListing";
+import axios from "axios";
 
+const config = {
+  headers: {
+    "x-api-key": "ec5fbc97-8313-4c5d-ac09-3e59940a364b",
+  },
+};
 function SearchCity() {
-    return (
-      <Container fluid style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '90vh',
-      }}>
-        <Row>
-          <Col>
-            <Card border="dark" style={{ width: "18rem"}}>
-              <Card.Header>Check a Property</Card.Header>
-            <Form style={{jusitfyContent: 'center', margin: '5px'}} className="col-10">
-            <Form.Group className="mb-3" controlId="inputCity">
-              <Form.Label>City</Form.Label>
-              <Form.Control type="text" placeholder="Optional" input="inputCity" />
-              <Form.Text className="text-muted">
-              </Form.Text>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="inputState">
-              <Form.Label>State</Form.Label>
-              <Form.Control type="text" placeholder="Required" id="inputState" required/>
-            </Form.Group>
-            <Button style={{justifyContent:'center'}} variant="primary" type="submit">
-              Submit
-            </Button>
-           </Form>
-           </Card>
-         </Col>
-    
+  const [state, setState] = useState("CA");
+  const [city, setCity] = useState("Berkeley");
+  const [cityResults, setCityResults] = useState();
+  const fetchCityDetails = async () => {
+    const url = `https://cors-anywhere.herokuapp.com/https://api.mashvisor.com/v1.1/client/city/investment//${state}/${city}`;
+    try {
+      const response = await axios.get(url, config);
+      setCityResults(response.data.content);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <Container
+      fluid
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "90vh",
+      }}
+    >
+      <Row>
         <Col>
-            <Card border="dark" style={{ width: "50rem" }} >
+          <Card border="dark" style={{ width: "18rem" }}>
+            <Card.Header>Check a Property</Card.Header>
+            <Form
+              style={{ jusitfyContent: "center", margin: "5px" }}
+              className="col-10"
+            >
+              <Form.Group className="mb-3" controlId="inputCity">
+                <Form.Label>City</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Optional"
+                  input="inputCity"
+                  value={city}
+                  onChange={(e) => setCity(e.target.value)}
+                />
+                <Form.Text className="text-muted"></Form.Text>
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="inputState">
+                <Form.Label>State</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Required"
+                  id="inputState"
+                  required
+                  value={state}
+                  onChange={(e) => setState(e.target.value)}
+                />
+              </Form.Group>
+              <Button
+                style={{ justifyContent: "center" }}
+                variant="primary"
+                onClick={fetchCityDetails}
+              >
+                Submit
+              </Button>
+            </Form>
+          </Card>
+        </Col>
+
+        {cityResults && (
+          <Col>
+            <Card border="dark" style={{ width: "50rem" }}>
               <div className="card">
                 <div className="card-body">
                   <h3 className="card-title">Area Stats</h3>
-                    <h4 className="areaStat"> Average Occupency Rate:</h4>
-                    <h5 className="abbStat">"Occupency Rate"</h5> 
-                    <h4 className="areaStat"> Number of Airbnbs:</h4>
-                    <h5 className="abbStat">"# of Airbnbs"</h5> 
-                    <h4 className="areaStat"> Average Nighlty Rate:</h4>
-                    <h5 className="abbStat">"Nightly Rate"</h5>
-                    <h4 className="areaStat"> Average Monthly Earning:</h4>
-                    <h5 className="abbStat">"Monthly Earnings"</h5>
-                    <h4 className="areaStat"> Average Property Price:</h4>
-                    <h5 className="abbStat">"Avg Property Price"</h5>   
-                    <a href="#" className="btn btn-outline-secondary btn-sm">
-                    <i className="far fa-heart"></i></a>
+                  <h4 className="areaStat">
+                    {" "}
+                    Average Occupency Rate:{cityResults.occupancy}
+                  </h4>
+                  <h5 className="abbStat">"Occupency Rate"</h5>
+                  <h4 className="areaStat"> Number of Airbnbs:</h4>
+                  <h5 className="abbStat">"# of Airbnbs"</h5>
+                  <h4 className="areaStat"> Average Nighlty Rate:</h4>
+                  <h5 className="abbStat">"Nightly Rate"</h5>
+                  <h4 className="areaStat"> Average Monthly Earning:</h4>
+                  <h5 className="abbStat">"Monthly Earnings"</h5>
+                  <h4 className="areaStat"> Average Property Price:</h4>
+                  <h5 className="abbStat">"Avg Property Price"</h5>
+                  <a href="#" className="btn btn-outline-secondary btn-sm">
+                    <i className="far fa-heart"></i>
+                  </a>
                 </div>
               </div>
-            </Card> 
+            </Card>
           </Col>
-        </Row>
-      </Container>
-    );
-};
+        )}
+      </Row>
+    </Container>
+  );
+}
 
 export default SearchCity;
--- server/models/property.js
+++ server/models/property.js
@@ -1,7 +1,13 @@
-const { Schema } = require('mongoose');
+const { Schema, STATES } = require('mongoose');
 
 const propertySchema = new Schema({
 
+avg-propertyprice
+   no-of-airbnbs
+  avg-occupency
+  airbnb-rental-rates-permonth
+STATE
+ciy
 });
 
 module.exports = propertySchema;
