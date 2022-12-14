b26106fd3aee9d00111fcdd8e1e37fbf2528cac5
Swetha Reddy <shwetha.reddy0@gmail.com>
2022-12-14 01:14:09 -0800

    fix client build path

--- server/server.js
+++ server/server.js
@@ -55,7 +55,7 @@ app.use((req, res, next) => {
 app.get("/cors", handleCors);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static("client/build"));
 }
 app.use(routes);
 
