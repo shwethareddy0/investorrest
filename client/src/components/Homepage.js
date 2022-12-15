26d4ce3024b71b113d4fa1a4ff62cd551d015560
Mrwaynejames <mcwilliamsjack96@gmail.com>
2022-12-14 18:25:40 -0800

    homepage

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -6,28 +6,26 @@ import { Nav } from "react-bootstrap";
 // Home page of the app
 function Homepage() {
   return (
-    <Container fluid className="col-8 mx-auto">
+    <Container fluid className="col-10 mx-auto">
       <Row>
         <Col>
           <Card
             className="mb-3"
-            style={{ width: "18rem", border: "2px solid lightgrey" }}
+            style={{ width: "17rem", border: "2px solid lightgrey" }}
           >
             <Card.Header className="ir-card-header">
-              Top 5 Cities in the USA
+              Top 5 Most Popular Cities in the USA
             </Card.Header>
             <ListGroup variant="flush">
-              <ListGroup.Item>Phoenix, AZ</ListGroup.Item>
-              <ListGroup.Item>Lake Tahoe, CA</ListGroup.Item>
-              <ListGroup.Item>Charleston, SC</ListGroup.Item>
-              <ListGroup.Item>Savannah, GA</ListGroup.Item>
-              <ListGroup.Item>Myrtle Beach, CA</ListGroup.Item>
-            </ListGroup>
-            <Card.Body>
+              <ListGroup.Item>Park City, UT</ListGroup.Item>
+              <ListGroup.Item>Breckenridge, CO</ListGroup.Item>
+              <ListGroup.Item>Steaamboat Springs, CO</ListGroup.Item>
+              <ListGroup.Item>Palm Springs, CA</ListGroup.Item>
+              <ListGroup.Item>Lahaina, HI</ListGroup.Item>
               <Nav.Link activeclassname="active" as={Link} to="/search">
                 Search
               </Nav.Link>
-            </Card.Body>
+            </ListGroup>
           </Card>
         </Col>
         <Col>
@@ -36,7 +34,7 @@ function Homepage() {
             style={{ width: "18rem", border: "2px solid lightgrey" }}
           >
             <Card.Header className="ir-card-header">
-              Top 5 States in the USA
+              Top 5 Most Popular States in the USA
             </Card.Header>
             <ListGroup variant="flush">
               <ListGroup.Item>Arizona</ListGroup.Item>
@@ -48,28 +46,22 @@ function Homepage() {
                 Search
               </Nav.Link>
             </ListGroup>
-            <Card.Body>
-              <Nav.Link activeclassname="active" as={Link} to="/search">
-                Search
-              </Nav.Link>
-            </Card.Body>
           </Card>
         </Col>
         <Col>
           <Card
             className="mb-3"
-            style={{ width: "18rem", border: "2px solid lightgrey" }}
+            style={{ width: "24rem", border: "2px solid lightgrey" }}
           >
             <Card.Header className="ir-card-header">
-              Airbnb by the #
+              Airbnb by the Numbers
             </Card.Header>
             <ListGroup variant="flush">
-              <ListGroup.Item>Total number of Airbnbs</ListGroup.Item>
-              <ListGroup.Item>Percentage over long term rental</ListGroup.Item>
-              <ListGroup.Item>Year over year growth</ListGroup.Item>
-              <ListGroup.Item>Average Occupancy</ListGroup.Item>
-              <ListGroup.Item>Return of Investment</ListGroup.Item>
-              <ListGroup.Item>Rental Rates</ListGroup.Item>
+              <ListGroup.Item>Total number of Airbnbs <p>5,600,000 listings</p></ListGroup.Item>
+              <ListGroup.Item>Profitability over long term rental <p>STR's can make 3.5x more</p></ListGroup.Item>
+              <ListGroup.Item>Year over year growth <p>32.56%</p></ListGroup.Item>
+              <ListGroup.Item>Average Occupancy<p>58.9%</p></ListGroup.Item>
+              <ListGroup.Item>Average Profits per Listing <p> $13,800/year</p></ListGroup.Item>
             </ListGroup>
           </Card>
         </Col>
